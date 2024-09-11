801cc86b39f6633ededf92900800e009c1280ad4
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-11 08:40:24 +0300

    more tomcat examples

---
+++ src/main/java/com/colak/springtutorial/config/TomcatConfig.java
@@ -0,0 +1,16 @@
+package com.colak.springtutorial.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+    }
+}
